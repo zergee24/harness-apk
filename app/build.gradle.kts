eb71cd156919cba9536c26747fa1200078100924
arc-zerg <17290685+arc-zerg@user.noreply.gitee.com>
2026-07-08 21:54:34 +0800

    发布：测试包升级到 0.1.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.harnessapk"
         minSdk = 26
         targetSdk = 37
-        versionCode = 15
-        versionName = "0.1.14"
+        versionCode = 16
+        versionName = "0.1.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
